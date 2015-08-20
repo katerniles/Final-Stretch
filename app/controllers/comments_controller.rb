1fa816993bcef1f5f2aaeca154aec70cec77fbed
Kate Russell <krussellnd@gmail.com>
2015-08-20 19:33:14 -0400

    add jQuery.fontFlex.js

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,9 @@ skip_authorize_resource :only => [:index, :show]
   def create
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
+
     @comment.user = current_user
+
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
