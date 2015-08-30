85fd7e873122945b910a96ef94ccabf6a5c7b53c
Kate Russell <krussellnd@gmail.com>
2015-08-30 08:29:20 -0400

    rspec errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  post '/rate' => 'rater#create', as => 'rate'
   devise_for :users
   resources :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
 
