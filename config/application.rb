9dec5923851fdfb73ecde02813e6c0033e488e9d
Kate Russell <krussellnd@gmail.com>
2015-09-06 17:33:23 -0400

    removed all redis

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'paper_trail'
-require 'redis_store'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,7 +10,6 @@ module Katesapp
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = false
-    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
 
   end
 end
