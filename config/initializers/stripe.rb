6e1fe6e7fea6f155cd8f3ecaac0968681853b35e
Kate Russell <krussellnd@gmail.com>
2015-09-07 17:59:29 -0400

    work on api

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,16 @@
 if Rails.env.production?
   Rails.configuration.stripe = {
-    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'] ||
-    "pk_test_ebHWWqUGlFUT8MoRRgsHsi04",
-    :secret_key      => ENV['STRIPE_SECRET_KEY']
-  }
-else
-  Rails.configuration.stripe = {
-    :publishable_key => 'STRIPE_PUBLISHABLE_KEY',
-    :secret_key      => 'STRIPE_SECRET_KEY'
+# 
+#    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'] ||
+#    "pk_test_ebHWWqUGlFUT8MoRRgsHsi04",
+#    :secret_key      => ENV['STRIPE_SECRET_KEY']
+ # }
+#else
+#  Rails.configuration.stripe = {
+#    :publishable_key => 'STRIPE_PUBLISHABLE_KEY',
+#    :secret_key      => 'STRIPE_SECRET_KEY'
+  :publishable_key => Rails.application.secrets.publishable_key
+  :secret_key      => Rails.application.secrets.secret_key
   }
 end
 
