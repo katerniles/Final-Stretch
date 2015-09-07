ab0f9acdb60f6256838541cbc7f5e82024c655a3
Kate Russell <krussellnd@gmail.com>
2015-09-07 18:43:45 -0400

    stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,5 +3,5 @@
     :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'] ||
     "pk_test_ebHWWqUGlFUT8MoRRgsHsi04",
     :secret_key      => ENV['STRIPE_SECRET_KEY']
-
+}
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
