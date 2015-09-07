ed3c6793b2c06850ada127dc7f6e19ecfa1ef9af
Kate Russell <krussellnd@gmail.com>
2015-09-07 19:03:50 -0400

    still working on api keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,10 @@
   Rails.configuration.stripe = {
 
-    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'] ||
-    "pk_test_ebHWWqUGlFUT8MoRRgsHsi04",
-    :secret_key      => ENV['STRIPE_SECRET_KEY']
+#    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'] ||
+#   "pk_test_ebHWWqUGlFUT8MoRRgsHsi04",
+#    :secret_key      => ENV['STRIPE_SECRET_KEY']
+  :publishable key => Rails.application.secrets.publishable_key
+  :secret key      => Rails.application.secrets.secret_key
+
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
