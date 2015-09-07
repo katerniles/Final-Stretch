dca0fcb3ee3ef1d6577eea8c89b48f932a2aff2c
Kate Russell <krussellnd@gmail.com>
2015-09-07 18:03:37 -0400

    comma in stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -9,9 +9,9 @@ if Rails.env.production?
 #  Rails.configuration.stripe = {
 #    :publishable_key => 'STRIPE_PUBLISHABLE_KEY',
 #    :secret_key      => 'STRIPE_SECRET_KEY'
-  :publishable_key => Rails.application.secrets.publishable_key
+  :publishable_key => Rails.application.secrets.publishable_key,
   :secret_key      => Rails.application.secrets.secret_key
-  }
+}
 end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
