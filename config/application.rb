1aafc0ba5cfb2ede418f1478344f62a76d849f00
Kate Russell <krussellnd@gmail.com>
2015-09-11 15:38:01 -0400

    config changes

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,7 @@
+config.before_configuration do
+  APP_CONFIG = YAML.load_file(File.expand_path('../config.yml', __FILE__))[Rails.env]
+  config.cache_store = :redis_store, APP_CONFIG['redis']
+end
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
@@ -9,7 +13,6 @@ module Katesapp
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = false
-    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
 
   end
 end
