faed013a0e936a23ccb800494ab2de1df7dbc7d8
Kate Russell <krussellnd@gmail.com>
2015-09-10 20:03:01 -0400

    config

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,11 @@ require 'paper_trail'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
 module Katesapp
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = false
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
 
   end
 end
