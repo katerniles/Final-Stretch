4ff461333a7593a2122a22e72f54f1e49346ea63
Kate Russell <krussellnd@gmail.com>
2015-09-12 13:57:40 -0400

    redis

--- config/initializers/redis_store.rb
+++
@@ -1,3 +0,0 @@
-redis_url = ENV["REDISCLOUD_URL"] || "redis://127.0.0.1:6379/0/katesapp2"
-katesapp2::Application.config.cache_store = :redis_store, redis_url
-katesapp2::Application.config.session_store :redis_store, redis_server: redis_url
